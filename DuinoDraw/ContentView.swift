cc6f6e257f6e4b411b924444a64d5ebd3f2dfbc9
Leo Kuo <p900372012@gmail.com>
2020-11-17 21:39:17 +0800

    Initial Commit

---
+++ DuinoDraw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DuinoDraw
+//
+//  Created by 郭家銘 on 2020/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
